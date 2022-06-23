6396f97a121803e0644d3b683a07bae7c0172841
朱世俊 <qq1034816916@163.com>
2022-06-23 15:43:18 +0800

    👏feat:sass添加

--- src/views/clock/index.vue
+++ src/views/clock/index.vue
@@ -1,7 +1,15 @@
 <template>
-    <h1>thats good</h1>
+    <div class="good">
+        <h1>thats good</h1>
+    </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.good {
+    h1 {
+        color: red;
+    }
+}
+</style>
