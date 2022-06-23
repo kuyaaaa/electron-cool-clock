6c2ece50a6acacf52e6268d40e5af5034401e6e2
朱世俊 <qq1034816916@163.com>
2022-06-23 16:25:06 +0800

    👏feat:时钟页面初代完成

--- src/views/clock/index.vue
+++ src/views/clock/index.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="good">
-        <h1>thats good</h1>
-    </div>
+    <span class="clock-text">
+        {{ time }}
+    </span>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import dayjs from "dayjs";
+
+const time = ref("");
+const updateTimer = ref<any>(null);
+
+const updateTime = () => {
+    const now = new Date();
+    time.value = dayjs(now).format("HH : mm : ss");
+};
+
+onMounted(() => {
+    updateTimer.value = setInterval(updateTime, 100);
+});
+
+onBeforeUnmount(() => {
+    clearInterval(updateTimer.value);
+});
+</script>
 
 <style lang="scss" scoped>
-.good {
-    h1 {
-        color: red;
-    }
+.clock-text {
+    font-size: 48px;
+    font-weight: 700;
+    color: #18a058;
+    cursor: default;
+    text-shadow: 2px 2px 4px #4d4e4e;
 }
 </style>
